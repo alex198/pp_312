3b461e3b1668795b27bfbcb793e600eb0fd0dbe7
alex198 <abykovskih@gmail.com>
2023-06-16 19:56:38 +0300

    внес корректировки в ссылку на методы update and delete

--- src/main/java/com/preproject/pp_3_1_2/controller/UsersController.java
+++ src/main/java/com/preproject/pp_3_1_2/controller/UsersController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,25 +29,22 @@ public class UsersController {
         model.addAttribute("users", userList);
         return "users";
     }
-
     @GetMapping("/user-create")
     public String createUserForm(User user) {
         return "user-create";
     }
-
     @PostMapping("/user-create")
     public String createUser(User user) {
         userService.saveUser(user);
         return "redirect:/users";
     }
-
-    @GetMapping("user-delete/{id}")
-    public String deleteUser(@PathVariable("id") Long id) {
-        userService.deleteUser(id);
+    @GetMapping("user-delete")
+    public String deleteUser(@RequestParam(value = "id") String id) {
+        userService.deleteUser(Long.parseLong(id));
         return "redirect:/users";
     }
-    @GetMapping("/user-update/{id}")
-    public String updateUserForm(@PathVariable("id") Long id, Model model) {
+    @GetMapping("/user-update")
+    public String updateUserForm(@RequestParam(value = "id") Long id, Model model) {
         User user = userService.getUserById(id);
         model.addAttribute("user", user);
         return "/user-update";
